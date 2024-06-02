4e1d168b470c7ccb35f62980d58fbd261e6bd1eb
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-03 00:56:31 +0530

    upgrade drizzle

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,30 +1,49 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   pgTableCreator,
   serial,
   timestamp,
   varchar,
   text,
+  integer,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `tribetie_${name}`);
 
-export const posts = createTable("questions", {
+export const communities = createTable("community", {
   id: serial("id").primaryKey(),
-  question: varchar("question", { length: 256 }).notNull().unique(),
-  dimension: varchar("dimension", { length: 256 }).notNull(),
-  choices: text("choices").array().notNull(),
+  name: varchar("name", { length: 256 }).notNull().unique(),
   createdAt: timestamp("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
   updatedAt: timestamp("updatedAt"),
 });
+
+export const communityRelations = relations(communities, ({ many }) => ({
+  posts: many(posts, { relationName: "posts" }),
+}));
+
+export const posts = createTable("post", {
+  id: serial("id").primaryKey(),
+  title: varchar("title", { length: 256 }).notNull(),
+  content: text("content").notNull(),
+  userId: varchar("userId", { length: 256 }).notNull(),
+  communityId: integer("community_id")
+    .references(() => communities.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt"),
+});
+
+export const postRelations = relations(posts, ({ one }) => ({
+  posts: one(communities, {
+    fields: [posts.communityId],
+    references: [communities.id],
+    relationName: "posts",
+  }),
+}));
