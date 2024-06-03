3d2e18e24e81be0b03998acc3e50621dbaf5f524
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-03 19:26:02 +0530

    auth api routes setup

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+
+import authOptions from "~/lib/auth";
+
+type Handler = () => Promise<Response>;
+
+const handler = NextAuth(authOptions) as Handler;
+
+export { handler as GET, handler as POST };
