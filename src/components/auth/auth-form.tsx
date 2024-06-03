cf59f4fbacac087de8c35179b4a0c88cb4d5f9fd
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-03 18:56:33 +0530

    auth components added

---
+++ src/components/auth/auth-form.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useState } from "react";
+
+import { Loader2 } from "lucide-react";
+import { signIn } from "next-auth/react";
+
+import { toast } from "~/hooks/use-toast";
+
+import { Icons } from "../icons";
+
+enum PROVIDER {
+  NULL = "",
+  GOOGLE = "google",
+  GITHUB = "github",
+}
+
+const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingProvider, setLoadingProvider] = useState<PROVIDER>(
+    PROVIDER.NULL,
+  );
+
+  const login = async (provider: PROVIDER) => {
+    setIsLoading(true);
+    setLoadingProvider(provider);
+
+    try {
+      await signIn(provider);
+    } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: `There was an error logging with ${provider}`,
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+      setLoadingProvider(PROVIDER.NULL);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center gap-4">
+      <button
+        onClick={() => void login(PROVIDER.GOOGLE)}
+        disabled={isLoading}
+        className="btn btn-primary btn-wide"
+      >
+        {loadingProvider === PROVIDER.GOOGLE ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{" "}
+        Google
+      </button>
+      <button
+        onClick={() => void login(PROVIDER.GITHUB)}
+        disabled={isLoading}
+        className="btn btn-primary btn-wide"
+      >
+        {loadingProvider === PROVIDER.GITHUB ? (
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.github className="mr-2 h-4 w-4" />
+        )}{" "}
+        Github
+      </button>
+    </div>
+  );
+};
+
+export default AuthForm;
