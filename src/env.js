ebc3eb6077daf1c1f586ff9b4de5fccbbdfeec79
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-03 22:47:39 +0530

    added oauth creds

--- src/env.js
+++ src/env.js
@@ -11,6 +11,9 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    NEXTAUTH_SECRET: z.string(),
   },
 
   /**
@@ -29,7 +32,9 @@ export const env = createEnv({
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,19 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import type { AuthOptions } from "next-auth";
 import type { Adapter } from "next-auth/adapters";
-// import GithubProvider from "next-auth/providers/github";
-// import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "~/server/db";
 import { env } from "~/env.js";
 
 const authOptions: AuthOptions = {
   adapter: DrizzleAdapter(db) as Adapter,
-  providers: [],
-  // providers: [
-  //   GoogleProvider({
-  //     clientId: env.GOOGLE_CLIENT_ID,
-  //     clientSecret: env.GOOGLE_CLIENT_SECRET,
-  //   }),
-  //   GithubProvider({
-  //     clientId: env.GITHUB_ID,
-  //     clientSecret: env.GITHUB_SECRET,
-  //   }),
-  // ],
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
   callbacks: {
     redirect() {
       return "/";
@@ -32,7 +26,7 @@ const authOptions: AuthOptions = {
     signIn: "/login",
   },
   debug: env.NODE_ENV === "development",
-  // secret: env.NEXTAUTH_SECRET,
+  secret: env.NEXTAUTH_SECRET,
 };
 
 export default authOptions;
