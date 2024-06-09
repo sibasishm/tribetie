a1bb2f1ed39a1fd9fcc24d5402562aaf942c871d
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-09 17:51:58 +0530

    community page

---
+++ src/app/t/[slug]/layout.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+
+import { format } from 'date-fns'
+import { and, eq, sql } from 'drizzle-orm'
+
+import {db} from '~/server/db'
+import { communities, subscriptions } from '~/server/db/schema'
+import { getCurrentUser } from '~/services/user'
+import SubscribeLeaveToggle from '~/components/subscribe-button'
+import ToFeedButton from '~/components/feed-back-button'
+
+type CommunityLayoutProps = {
+  children: React.ReactNode
+  params: {
+    slug: string
+  }
+}
+
+const CommunityLayout: React.FC<CommunityLayoutProps> = async ({
+  children,
+  params
+}) => {
+  const currentUser = await getCurrentUser()
+
+  const { slug } = params
+  const community = await db.query.communities.findFirst({
+    where: eq(communities.name, slug),
+    with: {
+      posts: {
+        with: {
+          author: true,
+          votes: true
+        }
+      }
+    }
+  })
+
+  if (!community) return notFound()
+
+  const subscription = currentUser
+    ? await db.query.subscriptions.findFirst({
+        where: and(
+          eq(subscriptions.communityId, community.id),
+          eq(subscriptions.userId, currentUser.id)
+        )
+      })
+    : undefined
+
+  const isSubscribed = !!subscription
+
+  const [memberCount] = await db
+    .select({
+      count: sql<number>`count(*)`
+    })
+    .from(subscriptions)
+    .where(eq(subscriptions.communityId, community.id))
+
+  return (
+    <div className='mx-auto h-full max-w-7xl pt-12 sm:container'>
+      <ToFeedButton />
+
+      <div className='grid grid-cols-1 gap-y-4 py-6 md:grid-cols-3 md:gap-x-4'>
+        <div className='col-span-2 flex flex-col space-y-6'>{children}</div>
+
+        {/* Info sidebar */}
+        <div className='order-first hidden h-fit overflow-hidden rounded-lg border border-gray-200 md:order-last md:block'>
+          <div className='px-6 py-4'>
+            <p className='py-3 font-semibold'>About r/{community?.name}</p>
+          </div>
+
+          <dl className='divide-y divide-gray-100 bg-white px-6 py-4 text-sm leading-6'>
+            <div className='flex justify-between gap-x-4 py-3'>
+              <dt className='text-gray-500'>Created</dt>
+              <dd className='text-gray-700'>
+                <time dateTime={community.createdAt?.toDateString()}>
+                  {format(community.createdAt!, 'MMMM d, yyyy')}
+                </time>
+              </dd>
+            </div>
+
+            <div className='flex justify-between gap-x-4 py-3'>
+              <dt className='text-gray-500'>Members</dt>
+              <dd className='text-gray-700'>
+                <div className='text-gray-900'>{memberCount?.count ?? 0}</div>
+              </dd>
+            </div>
+
+            {community.creatorId === currentUser?.id ? (
+              <div className='flex justify-between gap-x-4 py-3'>
+                <p className='text-gray-500'>You created this community</p>
+              </div>
+            ) : (
+              <SubscribeLeaveToggle
+                communityId={community.id}
+                communityName={community.name}
+                isSubscribed={isSubscribed}
+              />
+            )}
+
+            <Link
+              href={`/t/${slug}/submit`}
+              className='btn btn-primary btn-block'
+            >
+              Create post
+            </Link>
+          </dl>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CommunityLayout
---
+++ src/components/subscribe-button.tsx
@@ -0,0 +1,126 @@
+'use client'
+
+import React, { startTransition } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { useMutation } from '@tanstack/react-query'
+import ky, { HTTPError } from 'ky'
+import { Loader2 } from 'lucide-react'
+
+import type { SubscribeToCommunityPayload } from '~/lib/validators/community'
+import { useAuthToasts } from '~/hooks/use-auth-toats'
+import { toast } from '~/hooks/use-toast'
+
+type SubscribeLeaveToggleProps = {
+  communityId: number
+  communityName: string
+  isSubscribed: boolean
+}
+
+const SubscribeLeaveToggle: React.FC<SubscribeLeaveToggleProps> = ({
+  communityId,
+  communityName,
+  isSubscribed
+}) => {
+  const router = useRouter()
+
+  const { loginToast } = useAuthToasts()
+
+  const { mutate: subscribe, isPending: isSubLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToCommunityPayload = {
+        communityId
+      }
+
+      const data = await ky.post(
+        '/api/community/subscribe',
+        {
+          json: payload
+        }
+      ).json<string>()  
+
+      return data
+    },
+    onError: (error) => {
+      if (error instanceof HTTPError && error.response?.status === 401) {
+        return loginToast()
+      }
+
+      return toast({
+        title: 'There was a problem',
+        description: 'Something went wrong please try again',
+        variant: 'destructive'
+      })
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh()
+      })
+
+      return toast({
+        title: 'Subscribe',
+        description: `You are now subscribed to r/${communityName}`
+      })
+    }
+  })
+
+  const { mutate: unsubscribe, isPending: isUnsubLoading } = useMutation({
+    mutationFn: async () => {
+      const payload: SubscribeToCommunityPayload = {
+        communityId
+      }
+
+      const data = await ky.post(
+        '/api/community/unsubscribe',
+        {
+          json: payload
+        }
+      ).json<string>()
+
+      return data
+    },
+    onError: (error) => {
+      if (error instanceof HTTPError && error.response?.status === 401) {
+        return loginToast()
+      }
+
+      return toast({
+        title: 'There was a problem',
+        description: 'Something went wrong please try again',
+        variant: 'destructive'
+      })
+    },
+    onSuccess: () => {
+      startTransition(() => {
+        router.refresh()
+      })
+
+      return toast({
+        title: 'Unsubscribe',
+        description: `You are now unsubscribed from r/${communityName}`
+      })
+    }
+  })
+
+  return isSubscribed ? (
+    <button
+      onClick={() => void unsubscribe()}
+      disabled={isUnsubLoading}
+      className='btn mb-4 mt-1 btn-block'
+    >
+      {isUnsubLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />}{' '}
+      Leave community
+    </button>
+  ) : (
+    <button
+      onClick={() => void subscribe()}
+      disabled={isSubLoading}
+      className='btn mb-4 mt-1 btn-block'
+    >
+      {isSubLoading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />} Join
+      to post
+    </button>
+  )
+}
+
+export default SubscribeLeaveToggle
