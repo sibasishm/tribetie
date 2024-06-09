c727beee15a889ac809ca04ac2fabdf7f7b17759
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-09 17:40:45 +0530

    bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const HomePage = async () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold md:text-4xl">Your Feed</h1>
+      <h1 className="text-3xl font-bold md:text-4xl">{currentUser ? "Your Feed": "Trending Posts"}</h1>
       <div className="grid grid-cols-1 gap-y-4 py-6 md:grid-cols-3 md:gap-x-4">
         {currentUser ? <CustomFeed /> : <GeneralFeed />}
         <div className="order-first h-fit overflow-hidden rounded-lg border border-gray-200 md:order-last">
--- src/middleware.ts
+++ src/middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/t/:path*/submit", "/t/create", "/settings"],
+  matcher: ["/t/:path*/submit", "/t/create", "/settings", "/assessment/:path*"],
 };
