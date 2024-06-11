9d75b6104489dd2de7d192028890796f053db584
Sibasish Mohanty <smsibasish@gmail.com>
2024-06-12 02:18:29 +0530

    logo updated

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-12 w-12" />
+        <Icons.logo className="mx-auto h-6" />
         <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
         <p className="mx-auto max-w-xs text-sm">
           By continuing, you agree to our User Agreement and Privacy Policy.
--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -7,7 +7,7 @@ const Register = () => {
   return (
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
-        <Icons.logo className="mx-auto h-12 w-12" />
+        <Icons.logo className="mx-auto h-6" />
         <h1 className="text-2xl font-semibold tracking-tight">Sign up</h1>
         <p className="mx-auto max-w-xs text-sm">
           By continuing, you are setting up a TribeTie account and agree to our
